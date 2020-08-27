c7ca264a5aa0349a550949b26984dde43c0d2405
fanda-india <fanda.india@gmail.com>
2020-08-27 18:02:04 +0530

    New migration applied

--- Fanda/Fanda.Domain/Migrations/MySQL/20200827123033_MySQL02.Designer.cs
+++ Fanda/Fanda.Domain/Migrations/MySQL/20200827123033_MySQL02.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Fanda.Domain.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fanda.Domain.Migrations.MySQL
 {
     [DbContext(typeof(FandaContext))]
-    partial class FandaContextModelSnapshot : ModelSnapshot
+    [Migration("20200827123033_MySQL02")]
+    partial class MySQL02
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
