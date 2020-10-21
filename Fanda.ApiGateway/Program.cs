5fcb8993d0ab21d53996cfa7428285c7471c05ce
Fanda <58879581+fanda-india@users.noreply.github.com>
2020-10-21 13:33:41 +0530

    Everything like ApiGateway, Angular UI app, Authentication, Swagger set

--- Fanda.ApiGateway/Program.cs
+++ Fanda.ApiGateway/Program.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using Serilog;
 
 namespace Fanda.ApiGateway
 {
@@ -19,9 +22,22 @@ namespace Fanda.ApiGateway
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(config =>
+                .UseSerilog((builderContext, config) =>
                 {
-                    config.AddJsonFile("ocelot.json");
+                    config
+                        .MinimumLevel.Information()
+                        .Enrich.FromLogContext()
+                        .Enrich.WithMachineName()
+                        .Enrich.WithProperty("Application", Assembly.GetExecutingAssembly().FullName)
+                        .Enrich.WithEnvironmentUserName()
+                        .Enrich.WithThreadId()
+                        .WriteTo.Console();
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("ocelot.json", false, true);
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
--- Fanda.ApiGateway/Startup.cs
+++ Fanda.ApiGateway/Startup.cs
@@ -1,7 +1,9 @@
+using Fanda.Core;
+using Fanda.Core.Extensions;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,39 +25,18 @@ namespace Fanda.ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddCors(options =>
-            {
-                var urls = new[]
-                {
-                        "http://localhost:4200",    // Frontend Angular app from nodejs
-                        "http://localhost:55000",   // Frontend Angular app from http
-                        "http://localhost:5200",    // Accounting Service from http
-                        "http://localhost:5100",    // Authentication Service from http
-                        "http://localhost:5000",    // API Gateway from http
-                                                    // Configuration["Fanda.Gateway.Url"],
-                                                    // Configuration["Fanda.Ng.Url"]
-                };
-                options.AddPolicy("_MyAllowedOrigins", builder =>
-                {
-                    builder.WithOrigins(urls)
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
-                });
-            }); ; //WithViews();
-            services.AddResponseCaching();
+            AppSettings appSettings = services.ConfigureAppSettings(Configuration);
 
-            // In production, the Angular files will be served from this directory
-            //services.AddSpaStaticFiles(configuration =>
-            //{
-            //    configuration.RootPath = "ClientApp/dist";
-            //});
-            services.AddOcelot();
+            services.AddCustomControllers();
+            services.AddCustomCors();
+            services.AddOcelot(Configuration);
+            services.AddResponseCaching();
+            services.AddJwtAuthentication(appSettings.FandaSettings.Secret);
+            services.AddSwaggerForOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -69,39 +50,23 @@ namespace Fanda.ApiGateway
             }
 
             //app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
+
+            app.UseCors("_MyAllowedOrigins");
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseResponseCaching();
-            app.UseCors("_MyAllowedOrigins");
-            app.UseRouting();
-            app.UseOcelot();
 
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller}/{action=Index}/{id?}");
-            //    //.RequireCors("_MyAllowedOrigins");
-            //    // endpoints.MapHealthChecks("/health");
-            //});
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
 
-            //app.UseSpa(spa =>
-            //{
-            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //    // see https://go.microsoft.com/fwlink/?linkid=864501
-
-            //    spa.Options.SourcePath = "ClientApp";
-
-            //    if (env.IsDevelopment())
-            //    {
-            //        // spa.UseAngularCliServer(npmScript: "start");
-            //        spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-            //    }
-            //});
+            await app.UseOcelot();
         }
     }
 }
